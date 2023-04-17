06bdee1f24b26c9150d5dc9801f810cd615052a8
Grant Huiras <huiras.grant@gmail.com>
2023-04-17 08:59:35 -0500

    Created Universal Update Script
    This universal update will update Arch, Debian, & Ubuntu distributions.

--- practice/while.sh
+++ practice/while.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-myvar=1
-
-while [ $myvar -le 10 ]
+while [ -f ~/testfile ]
 do
-    echo $myvar
-    myvar=$(( $myvar +1 ))
-    sleep 0.5
+    echo "As of $(date), the file exists."
+    sleep 5
 done
+
+echo "As of $(date), the file does not exist."
